74fa1fdcfc27a2917721058fd2adc466ec1839c0
김태연/Erin_Kim <76546136+Erin2021@users.noreply.github.com>
2024-06-03 01:55:35 +0900

    Update background.js

--- js/background.js
+++ js/background.js
@@ -3,5 +3,5 @@ const images =["0.jpg","1.jpg","2.jpg","3.jpg","4.jpg"];
 const chosenImage =images[Math.floor(Math.random()*images.length)];
 
 const bgImage= document.querySelector("body");
-bgImage.style.background=`url(./img/${chosenImage}) no-repeat left top / cover`;
+bgImage.style.background=`url(${chosenImage}) no-repeat left top / cover`;
 
